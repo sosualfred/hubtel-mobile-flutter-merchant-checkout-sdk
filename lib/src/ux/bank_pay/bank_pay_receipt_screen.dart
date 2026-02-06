3d29a90360eef54f56f1c96c030a8dd4ca62d8e8
Sosu Alfred <sosualfred@gmail.com>
2026-02-06 14:44:54 +0000

    feat: Update package description, Flutter SDK constraints, and replace the PDF generation dependency with `html_to_pdf_plus`.

--- lib/src/ux/bank_pay/bank_pay_receipt_screen.dart
+++ lib/src/ux/bank_pay/bank_pay_receipt_screen.dart
@@ -1,9 +1,8 @@
 import 'dart:developer';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
+import 'package:html_to_pdf_plus/html_to_pdf_plus.dart';
 
-// import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
 import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -15,16 +14,14 @@ import '../../resources/html_strings.dart';
 import 'bank_pay_status_screen.dart';
 
 class BankPayReceiptScreen extends StatefulWidget {
-  String generatedPdfFilePath = '';
+  const BankPayReceiptScreen({
+    Key? key,
+    required this.mobileMoneyResponse,
+    required this.businessDetails,
+  }) : super(key: key);
 
-  BankPayReceiptScreen(
-      {Key? key,
-      required this.mobileMoneyResponse,
-      required this.businessDetails})
-      : super(key: key);
-
+  final String generatedPdfFilePath = '';
   final MomoResponse mobileMoneyResponse;
-
   final HtmlRequirements businessDetails;
 
   @override
@@ -38,8 +35,12 @@ class _BankPayReceiptScreenState extends State<BankPayReceiptScreen> {
   void initState() {
     super.initState();
     controller = WebViewController()
-      ..loadHtmlString(HTMLStrings.generateString(
-          widget.businessDetails, widget.mobileMoneyResponse));
+      ..loadHtmlString(
+        HTMLStrings.generateString(
+          widget.businessDetails,
+          widget.mobileMoneyResponse,
+        ),
+      );
   }
 
   @override
@@ -48,32 +49,36 @@ class _BankPayReceiptScreenState extends State<BankPayReceiptScreen> {
       title: "Download Pdf",
       pageDecoration: PageDecoration(
         backgroundColor: HubtelColors
-            .neutral.shade300, // Theme.of(context).scaffoldBackgroundColor,
+            .neutral
+            .shade300, // Theme.of(context).scaffoldBackgroundColor,
       ),
       hideBackNavigation: false,
       bottomNavigation: Container(
         color: Colors.white,
         padding: const EdgeInsets.all(16),
         child: CustomButton(
-            title: 'Download Pdf'.toUpperCase(),
-            isEnabled: true,
-            buttonAction: () => {
-                  generatePdfFromHtml(HTMLStrings.generateString(
-                      widget.businessDetails, widget.mobileMoneyResponse))
-                },
-            loading: false,
-            isDisabledBgColor: HubtelColors.lighterGrey,
-            disabledTitleColor: HubtelColors.grey,
-            style: HubtelButtonStyle.solid,
-            isEnabledBgColor: ThemeConfig.themeColor),
+          title: 'Download Pdf'.toUpperCase(),
+          isEnabled: true,
+          buttonAction: () => {
+            generatePdfFromHtml(
+              HTMLStrings.generateString(
+                widget.businessDetails,
+                widget.mobileMoneyResponse,
+              ),
+            ),
+          },
+          loading: false,
+          isDisabledBgColor: HubtelColors.lighterGrey,
+          disabledTitleColor: HubtelColors.grey,
+          style: HubtelButtonStyle.solid,
+          isEnabledBgColor: ThemeConfig.themeColor,
+        ),
       ),
       body: SafeArea(
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(Dimens.paddingDefault),
-            child: WebViewWidget(
-              controller: controller,
-            ),
+            child: WebViewWidget(controller: controller),
           ),
         ),
       ),
@@ -89,22 +94,27 @@ class _BankPayReceiptScreenState extends State<BankPayReceiptScreen> {
     final targetFileName =
         "${widget.businessDetails.businessName}_${widget.mobileMoneyResponse.customerName}_${DateTime.now()}";
 
-    final generatedPdfFile = await FlutterHtmlToPdf.convertFromHtmlContent(
-        htmlContent, targetPath ?? "", targetFileName);
+    final generatedPdfFile = await HtmlToPdf.convertFromHtmlContent(
+      configuration: PdfConfiguration(
+        targetDirectory: targetPath,
+        targetName: targetFileName,
+        printSize: PrintSize.A4,
+        printOrientation: PrintOrientation.Portrait,
+        linksClickable: true,
+      ),
+      htmlContent: htmlContent,
+    );
     log('$generatedPdfFile', name: '$runtimeType');
 
-    final openFile = await OpenFilex.open(generatedPdfFile.path);
+    await OpenFilex.open(generatedPdfFile.path);
 
     if (!mounted) return;
 
     Navigator.pop(context);
 
-    if (openFile != null) {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => const BankPayStatusScreen()));
-    }
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const BankPayStatusScreen()),
+    );
   }
-
-
-
 }
